19027fda3d8a9ecff8c064619ab8a97fbdaf29d7
shibattor <87258997+shibattor@users.noreply.github.com>
2022-11-01 18:32:39 +0100

    final version

--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
-﻿void Countdown() {
+﻿bool DivisibleBy(int number, int divident) {
+    return number % divident == 0;
+}
+
+void Countdown() {
     for (int i = 100; i > 0; i--)
     {
-        Console.WriteLine($"Number: {i}");
+        //Console.WriteLine($"Debug info: current number: {i}");
+
+        var divisibleBy3 = DivisibleBy(i, 3);
+        var divisibleBy5 = DivisibleBy(i, 5);
+
+        if (divisibleBy3 && divisibleBy5)
+        {
+            Console.WriteLine($"Testing");            
+        } 
+        else
+        {
+            if (divisibleBy3)
+            {
+                Console.WriteLine($"Software");      
+                continue;  
+            }
+            if (divisibleBy5)
+            {
+                Console.WriteLine($"Agile");   
+                continue;  
+            }
+            
+            Console.WriteLine($"{i}");            
+        }
     }
 }
 
